1cdffd803c35f66adc845a9cf675b739eadd5865
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-10-14 07:36:50 +0200

    add initial sina backend

---
+++ sina-backend/src/main/java/pl/lodz/p/sina/sinabackend/SinaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.sina.sinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinaBackendApplication.class, args);
+    }
+
+}
---
+++ sina-backend/src/test/java/pl/lodz/p/sina/sinabackend/SinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.sina.sinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
